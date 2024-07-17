9c517f44d9500eea79831377e7ea00f2f9b83729
Rusell Pan <russell1997100188@gmail.com>
2024-07-17 11:30:45 +1000

    fix carousel issue and add simpleui for management system

--- Home_App/admin.py
+++ Home_App/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
+admin.site.site_header = 'Feane'
+admin.site.site_title = 'Feane Management'
+admin.site.index_title = 'Feane Management'
+
 from Home_App.models import *
 
 # Register your models here.
